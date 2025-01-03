6264e721a3bab26c049d79bd3e225f62416bdf66
Aashwija Thantry <aashwija.thantry@gmail.com>
2025-01-03 18:00:01 +0530

    row counter added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let count = 0;
+let rowCount = 0;
 
+// Stitch counter
 document.getElementById("dec").onclick=function(){
     if(count==0) {
         return count;
@@ -10,11 +12,35 @@ document.getElementById("dec").onclick=function(){
     
     document.getElementById("countLabel").innerHTML=count;
 }
+
 document.getElementById("res").onclick=function(){
     count=0;
     document.getElementById("countLabel").innerHTML=count;
 }
+
 document.getElementById("inc").onclick=function(){
     count+=1;
     document.getElementById("countLabel").innerHTML=count;
+}
+
+// Row Counter
+document.getElementById("dec1").onclick=function(){
+    if(rowCount==0) {
+        return rowCount;
+    }
+    else {
+        rowCount-=1;
+    }
+    
+    document.getElementById("rowLabel").innerHTML=rowCount;
+}
+
+document.getElementById("res1").onclick=function(){
+    rowCount=0;
+    document.getElementById("rowLabel").innerHTML=rowCount;
+}
+
+document.getElementById("inc1").onclick=function(){
+    rowCount+=1;
+    document.getElementById("rowLabel").innerHTML=rowCount;
 }
